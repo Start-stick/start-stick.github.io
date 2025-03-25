b4924981f97d810bf586d778495f9825443c1548
upupupupu <1711603974@qq.com>
2025-03-25 23:08:58 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
@@ -20,7 +21,6 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
-  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
